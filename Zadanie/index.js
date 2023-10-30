77627be91283d25245dc30d1f495c2ed5334298a
Zuravl. <dmitrijzuravlev36357@gmail.com>
2023-10-30 10:09:34 +0300

    Zadanie mb

---
+++ Zadanie/index.js
@@ -0,0 +1,48 @@
+const SguareNumber = (x) => {
+    let result = x * x;
+    return result;
+}
+
+const isSguare = (a, b) => {
+    let result = false;
+    if (b*b===a) result = true;
+    return resylt;
+}
+
+const minimumNumber = (a, b, c) => {
+    let result = 0;
+    result = Math.min(a, b, c)
+    return result;
+} 
+
+const maxmumNumber = (a, b, c) => {
+    let result = 0;
+    result = Math.max(a, b, c);
+    return result;
+}
+
+const seguenceGenerator = (value) => {
+    let result = '';
+    for (let i = 0; i < value.length; i+=1);
+    result = result + '' + i;
+    result = result.trim();
+    return result;
+}
+
+const evenNumbers = (value) => {
+    let result = '';
+    for (let i = 0; i < value.length; i+=2);
+    result = result + '' + i;
+    return substring(1);
+}
+
+const getSum = (a, b) => {
+    return a + b;
+}
+ const getSmollerNumber = (a, b) => {
+    return Math.min(a,b);
+ }
+
+const castBoolean = (value) => {
+    return Boolean(value);
+}
